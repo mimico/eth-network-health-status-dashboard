0c72a4dd7d96ef28e8b7a1267d9308f85c7c9e5c
Tiffany ANTOPOLSKI <tiffany.antopolski@gmail.com>
2019-02-07 13:22:06 -0500

    Present last block mined in DOM

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,31 @@
 let fetchData = () => {
 
-  //example: {"jsonrpc":"2.0","id":83,"result":"0x6d918b"}
-  let url = "https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=YourApiKeyToken";
-  console.log(url);
-  let stuff =  fetch(url); //return a promise.
-  console.log (stuff);  //got a promise :-)
-  return stuff;
+  /* get the number of the last block mined */
+  let fetchLastBlock = () => {
+    //example: {"jsonrpc":"2.0","id":83,"result":"0x6d918b"}
+    let url = "https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=YourApiKeyToken";
+    let data =  fetch(url); //return a promise.
+    return data;
+  }//end fetchLastBlock
+
+  fetchLastBlock().then(response => {
+    /* we get a Promise, so we need to use a then method to
+    manipulate a response from that promise */
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error("Api did not respond");
+  }).then(response => {
+    /* process json for data required */
+    //console.log("response: " + response);
+    lastBlockMined = parseInt(response.result, 16); //parse for dec from hex
+    var access = document.getElementById("lastBlock");
+    access.innerHTML += lastBlockMined;
+  })
+
+  /* get the last ether price in btc and usd */
+
+  /* marketCap = totalEther * price */
 
 }//end fetchData
-
-fetchData().then(response => {
-  /* we get a Promise, so we need to use a then method to
-  manipulate a response from that promise */
-  console.log(response);
-  if (response.ok) {
-    return response.json();
-  }
-  throw new Error("Api did not respond");
-}).then(response => {
-  // process json for data required
-  console.log("response: " + response);
-  var lastBlockMinedHex = response.result; //an array of objects
-  console.log("lastBlockMinedHex: " + lastBlockMinedHex);
-  console.log("lastBlockMinedDec: " + parseInt(lastBlockMinedHex, 16));
-})
-
-
 window.onload = fetchData();
