f014d13dbf66540cf7054223a10d1a2de4ecaf4e
Tiffany ANTOPOLSKI <tiffany.antopolski@gmail.com>
2019-02-09 15:16:22 -0500

    Decrease line length

--- scripts/script.js
+++ scripts/script.js
@@ -3,23 +3,31 @@ let fetchData = () => {
   /* get the number of the last block mined */
   let fetchLastBlock = () => {
     //example: {"jsonrpc":"2.0","id":83,"result":"0x6d918b"}
-    let url = "https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=YourApiKeyToken";
+    let url = "https://api.etherscan.io/api?module=proxy&action=\
+eth_blockNumber&apikey=YourApiKeyToken";
     let data =  fetch(url); //returns a promise.
     return data;
   }//end fetchLastBlock
 
   /* get the last ether price in btc and usd */
   let fetchEthPrice = () => {
-    //{"status":"1","message":"OK","result":{"ethbtc":"0.03077","ethbtc_timestamp":"1549581188","ethusd":"104.62","ethusd_timestamp":"1549581181"}}
-    let url = "https://api.etherscan.io/api?module=stats&action=ethprice&apikey=YourApiKeyToken";
+    /* example: {"status":"1","message":"OK",
+     * "result":{"ethbtc":"0.03077","ethbtc_timestamp":"1549581188",
+     * "ethusd":"104.62","ethusd_timestamp":"1549581181"}}
+     */
+    let url = "https://api.etherscan.io/api?module=stats&action=\
+ethprice&apikey=YourApiKeyToken";
     let data =  fetch(url); //returns a promise.
     return data;
   }//end fetchLastBlock
 
   /*get total supply of ether*/
   let fetchTotalEther = () => {
-    //example: {"status":"1","message":"OK","result":"104764826749100000000000000"}
-    let url = "https://api.etherscan.io/api?module=stats&action=ethsupply&apikey=YourApiKeyToken";
+    /* example:
+     * {"status":"1","message":"OK","result":"104764826749100000000000000"}
+     */
+    let url = "https://api.etherscan.io/api?module=stats&action=\
+ethsupply&apikey=YourApiKeyToken";
     let data =  fetch(url); //returns a promise.
     return data;
   }//end fetchtotalEther
@@ -41,19 +49,23 @@ let fetchData = () => {
         })).then (res => {
               switch(i) {
                 case 0:
-                  lastBlockMined = parseInt(res.result.result, 16); //parse from hex to dec
-                  document.getElementById("lastBlock").innerHTML = "last block: " + lastBlockMined;
+                  //parse from hex to dec
+                  lastBlockMined = parseInt(res.result.result, 16);
+                  document.getElementById("lastBlock").innerHTML = "last block: "
+                                                              + lastBlockMined;
                   break;
                 case 1:
                   ethUSDprice = res.result.result.ethusd;
                   ethBTCprice = res.result.result.ethbtc;
-                  document.getElementById("price").innerHTML = "$" + ethUSDprice + " @ " + ethBTCprice + " btc/eth";
+                  document.getElementById("price").innerHTML = "$"
+                  + ethUSDprice + " @ " + ethBTCprice + " btc/eth";
                   break;
                 case 2:
                   totalWei = res.result.result
                   totalEther = totalWei/10e17;
                   marketCap = totalEther * ethUSDprice;
-                  document.getElementById("marCap").innerHTML = "market cap of $" + (marketCap/10e8).toFixed(3) + " billion";
+                  document.getElementById("marCap").innerHTML = "market cap of $"
+                                    + (marketCap/10e8).toFixed(3) + " billion";
                   break;
               }
             });
